1103dda909bb54864f81f504f3692da519450519
zeineb <zeineb.chelly@gmail.com>
2014-12-21 15:59:52 +0100

    Update SampleFuzzyUse.pl
    I have changed the example to let the context be more clear. I have changed the capital letters to lowercases for the variable names as you stated in your previous comment and I have added the strict and warning statements. I have 0 errors. Now all is fine file with this module :)
    As you said the parametrization of this module should be kept in a separate file. We can do this later once we decide what is our next step.

--- Algorithm-Fuzzy-Chelly/app/SampleFuzzyUse.pl
+++ Algorithm-Fuzzy-Chelly/app/SampleFuzzyUse.pl
@@ -3,16 +3,25 @@
 use strict;
 use warnings;
 
-##########################################################################################################
+
+=head1
+ 
+ Defining the problem : 
+ 
+ Given a number between 0 and 25 that represents the quality of service at a restaurant 
+ (where 25 is excellent), and another number between 0 and 25 that represents the
+ quality of the food at that restaurant (again, 25 is excellent), what should the tip be?
+
+=cut
+
+
 #****************************DEFINING THE RANGE : UNIVERSE OF DISCOURSE [0..25] For example **************
 
 
-#Global Variables
+  my $min_range = 0;
+  my $max_range = 25;
 
-my $min_range = 0;
-my $max_range = 25;
-
-my $range = $max_range - $min_range;
+  my  $range = $max_range - $min_range;
 
 
 ##########################################################################################################
@@ -22,76 +31,89 @@ my $range = $max_range - $min_range;
 ##########################################################################################################
 #**********************************************************************************************************
 
-						#Input-Output Description 
+=head2					
+                          	Input-Output Description 
 
+  ---> In this sample, we will use 2 inputs and 1 output. Their names are as follows:
+  input 1 =  $service
+  input 2 = $food
+  output =  $tip
 
-# ---> In this sample, we will use 2 inputs and 1 output
+=cut
+##########################################################################################################
+
+#Linguistic Variable for input 1 =  $service : Definied with 3 linguistic triangular membership functions (3 terms sets)
+  my $service_poor; 
+  $service_poor->{0} = 0;
+  $service_poor->{5} = 1;
+  $service_poor->{10} = 0;
+  $service_poor = transform_step1_range($min_range,$max_range,$service_poor);
+  $service_poor = generate_values_membership_form($service_poor);
+
+ my  $service_medium;
+  $service_medium->{5} = 0;
+  $service_medium->{10} = 1;
+  $service_medium->{15} = 0;
+  $service_medium = transform_step1_range($min_range,$max_range,$service_medium);
+  $service_medium = generate_values_membership_form($service_medium);
+
+  my $service_excellent;
+  $service_excellent->{6} = 0;
+  $service_excellent->{17} = 1;
+  $service_excellent->{25} = 0;
+  $service_excellent = transform_step1_range($min_range,$max_range,$service_excellent);
+  $service_excellent = generate_values_membership_form($service_excellent);
 
 ##########################################################################################################
 
-#Linguistic Variable for input 1 =  $mature_input1 : Definied with 3 linguistic triangular membership functions (3 terms sets)
+#Linguistic Variable for input 2 = $food : Definied with 3 linguistic triangular membership functions (3 terms sets)
+
+  my $food_rancid;
+  $food_rancid->{0} = 0;
+  $food_rancid->{5} = 1;
+  $food_rancid->{10} = 0;
+  $food_rancid = transform_step1_range($min_range,$max_range,$food_rancid);
+  $food_rancid = generate_values_membership_form($food_rancid);
+
+  my $food_medium;
+  $food_medium->{5} = 0;
+  $food_medium->{10} = 1;
+  $food_medium->{15} = 0;
+  $food_medium = transform_step1_range($min_range,$max_range,$food_medium);
+  $food_medium = generate_values_membership_form($food_medium);
+
  
-my $Mature_Low->{0} = 0;
-my $Mature_Low->{5} = 1;
-my $Mature_Low->{10} = 0;
-my $Mature_Low = Transform_Step1_RANGE($min_range,$max_range,$Mature_Low);
-my $Mature_Low = Generate_Values_Membership_Form($Mature_Low);
-
-
-my $Mature_Medium->{7} = 0;
-my $Mature_Medium->{12} = 1;
-my $Mature_Medium->{17} = 0;
-my $Mature_Medium = Transform_Step1_RANGE ($min_range,$max_range,$Mature_Medium);
-my $Mature_Medium = Generate_Values_Membership_Form($Mature_Medium);
-
-
-my $Mature_High->{15} = 0;
-my $Mature_High->{20} = 1;
-my $Mature_High->{25} = 0;
-my $Mature_High = Transform_Step1_RANGE ($min_range,$max_range,$Mature_High);
-my $Mature_High = Generate_Values_Membership_Form($Mature_High);
+  my $food_delicious;
+  $food_delicious->{6} = 0;
+  $food_delicious->{17} = 1;
+  $food_delicious->{25} = 0;
+  $food_delicious = transform_step1_range($min_range,$max_range,$food_delicious);
+  $food_delicious = generate_values_membership_form($food_delicious);
 
 ##########################################################################################################
 
-#Linguistic Variable for input 1 = $semimature_input2 : Definied with 3 linguistic triangular membership functions (3 terms sets)
+#Linguistic Variable 3 (output 1) =  $tip  definied with 3 linguistic triangular membership functions (3 terms sets)
 
-my $SemiMature_Low->{0} = 0;
-my $SemiMature_Low->{5} = 1;
-my $SemiMature_Low->{10} = 0;
-my $SemiMature_Low = Transform_Step1_RANGE ($min_range,$max_range,$SemiMature_Low);
-my $SemiMature_Low = Generate_Values_Membership_Form($SemiMature_Low);
+  my $tip_cheap;
+  $tip_cheap->{0} = 0;
+  $tip_cheap->{5} = 1;
+  $tip_cheap->{10} = 0;
+  $tip_cheap = transform_step1_range($min_range,$max_range,$tip_cheap);
+  $tip_cheap = generate_values_membership_form($tip_cheap);
 
+ my $tip_average;
+ $tip_average->{5} = 0;
+ $tip_average->{10} = 1;
+ $tip_average->{15} = 0;
+ $tip_average = transform_step1_range($min_range,$max_range,$tip_average);
+ $tip_average = generate_values_membership_form($tip_average);
 
-my $SemiMature_Medium->{7} = 0;
-my $SemiMature_Medium->{12} = 1;
-my $SemiMature_Medium->{17} = 0;
-my $SemiMature_Medium = Transform_Step1_RANGE ($min_range,$max_range,$SemiMature_Medium);
-my $SemiMature_Medium = Generate_Values_Membership_Form($SemiMature_Medium);
-
-
-my $SemiMature_High->{15} = 0;
-my $SemiMature_High->{20} = 1;
-my $SemiMature_High->{25} = 0;
-my $SemiMature_High = Transform_Step1_RANGE ($min_range,$max_range,$SemiMature_High);
-my $SemiMature_High = Generate_Values_Membership_Form($SemiMature_High);
-
-##########################################################################################################
-
-#Linguistic Variable 3 (output 1) =  $MATURITY  definied with 2 linguistic triangular membership functions (2 terms sets)
-
-
-my $Maturity_SemiMature->{0} = 0;
-my $Maturity_SemiMature->{8} = 1;
-my $Maturity_SemiMature->{15} = 0;
-my $Maturity_SemiMature = Transform_Step1_RANGE ($min_range,$max_range,$Maturity_SemiMature);
-my $Maturity_SemiMature = Generate_Values_Membership_Form($Maturity_SemiMature);
-
-
-my $Maturity_Mature->{10} = 0;
-my $Maturity_Mature->{18} = 1;
-my $Maturity_Mature->{25} = 0;
-my $Maturity_Mature = Transform_Step1_RANGE ($min_range,$max_range,$Maturity_Mature);
-my $Maturity_Mature = Generate_Values_Membership_Form($Maturity_Mature);
+ my $tip_generous; 
+ $tip_generous->{8} = 0;
+ $tip_generous->{17} = 1;
+ $tip_generous->{25} = 0;
+ $tip_generous = transform_step1_range($min_range,$max_range,$tip_generous);
+ $tip_generous = generate_values_membership_form($tip_generous);
 
 ##########################################################################################################
 
@@ -107,140 +129,180 @@ my $Maturity_Mature = Generate_Values_Membership_Form($Maturity_Mature);
 
 ############### Getting the values $Vble_1 and $Vble_2 in the specified $range   ##########################################
 
-#Vbles to fuzzify $Vble1 & $Vble2
+#Vbles to fuzzify $service & $food
 
-my $Vble_1 = 8;
-my $Vble_2 = 9;
+my  $service = 7;
+my  $food  = 9;
 
+print " The Variables to fuzzify are:\n";
 
-my $mature_input1 = ScalingToTheRange($min_range,$max_range,$Vble_1);
+ $service = scaling_to_the_range($min_range,$max_range,$service);
 
-print "  $mature_input1\n";
+print "  $service\n";
 
-my $semimature_input2 = ScalingToTheRange($min_range,$max_range,$Vble_2);
+ $food = scaling_to_the_range($min_range,$max_range,$food);
 
-print "  $semimature_input2\n";
+print "  $food\n";
 
 
 ############### The Input's Membership Function Values : Fuzzification ###############################
 
 
-my $Fuzzy_Mature_Low = $Mature_Low->{$mature_input1};
+ my $fuzzy_service_poor = $service_poor->{$service};
+ print " fuzzy_service_poor = $fuzzy_service_poor\n";
 
-print " Fuzzy_Mature_Low = $Fuzzy_Mature_Low\n";
-
-my $Fuzzy_Mature_Medium = $Mature_Medium->{$mature_input1};
-
-print " Fuzzy_Mature_Medium = $Fuzzy_Mature_Medium\n";
+my  $fuzzy_service_medium = $service_medium->{$service};
+ print " Fuzzy_service_medium = $fuzzy_service_medium\n";
  
-my $Fuzzy_Mature_High = $Mature_High->{$mature_input1};
+my  $fuzzy_service_excellent = $service_excellent->{$service};
+ print " Fuzzy_service_excellent = $fuzzy_service_excellent\n";
 
-print " Fuzzy_Mature_High = $Fuzzy_Mature_High\n";
+my $fuzzy_food_rancid = $food_rancid->{$food};
+ print " Fuzzy_food_rancid = $fuzzy_food_rancid\n";
 
-
-
-my $Fuzzy_SemiMature_Low = $SemiMature_Low->{$semimature_input2};
-
-print " Fuzzy_SemiMature_Low = $Fuzzy_SemiMature_Low\n";
-
-my $Fuzzy_SemiMature_Medium = $SemiMature_Medium->{$semimature_input2};
-
-print " Fuzzy_SemiMature_Medium = $Fuzzy_SemiMature_Medium\n";
+my $fuzzy_food_medium = $food_medium->{$food};
+ print " Fuzzy_food_medium = $fuzzy_food_medium\n";
  
-my $Fuzzy_SemiMature_High = $SemiMature_High->{$semimature_input2};
-
-print " Fuzzy_SemiMature_High = $Fuzzy_SemiMature_High\n";
-
+my $fuzzy_food_delicious = $food_delicious->{$food};
+ print " Fuzzy_food_delicious = $fuzzy_food_delicious\n";
+ 
 
 ###############   Apply Fuzzy Operator : Aggregation :   "AND" OPERATOR  ###############################
+=head3
+  
+  --->Rules Description:
 
-# Define the operator for Aggregation : ("AND"  ,  "OR"  , "PROD") 
+1 If the service is poor and the food is rancid, then tip is cheap.
+2 If the service is poor and the food is medium, then tip is cheap.
+3 If the service is poor and the food is delicious, then tip is average.
 
-my $Op_Aggregation = "AND";
+4 If the service is medium and the food is rancid, then tip is cheap.
+5 If the service is medium and the food is medium, then tip is average.
+6 If the service is medium and the food is delicious, then tip is average.
+
+7 If the service is excellent and the food is rancid, then tip is average.
+8 If the service is excellent and the food is medium, then tip is generous.
+9 If the service is excellent and the food is delicious, then tip is generous.
+
+=cut
+#############################################################################################################
 
 # Aggragation phase
 
+# Define the operator for Aggregation : ("AND"  ,  "OR"  , "PROD") 
 
-my $rule1 = Fuzzy_Aggregation_Step($Op_Aggregation ,$Fuzzy_Mature_Low,$Fuzzy_SemiMature_Low);
-
-print " rule1 = $rule1\n";
-
+my $op_aggregation = "AND";
+my $rule1 = fuzzy_aggregation_step($op_aggregation ,$fuzzy_service_poor,$fuzzy_food_rancid);
+ print " rule1 = $rule1\n";
 my $weight_rule1 = 1;
 
-my $rule2 = Fuzzy_Aggregation_Step($Op_Aggregation ,$Fuzzy_Mature_Low,$Fuzzy_SemiMature_Medium);
-
-print " rule2 = $rule2\n";
-
+ $op_aggregation = "AND";
+my $rule2 = fuzzy_aggregation_step($op_aggregation ,$fuzzy_service_poor,$fuzzy_food_medium);
+ print " rule2 = $rule2\n";
 my $weight_rule2 = 1;
 
-
-my $rule3 = Fuzzy_Aggregation_Step($Op_Aggregation ,$Fuzzy_Mature_Medium,$Fuzzy_SemiMature_Low);
-
-print " rule3 = $rule3\n";
-
+ $op_aggregation = "AND";
+my $rule3 = fuzzy_aggregation_step($op_aggregation ,$fuzzy_service_poor,$fuzzy_food_delicious);
+ print " rule3 = $rule3\n";
 my $weight_rule3 = 1;
 
-
-my $rule4 = Fuzzy_Aggregation_Step($Op_Aggregation ,$Fuzzy_Mature_Medium,$Fuzzy_SemiMature_Medium);
-
-print " rule4 = $rule4\n";
-
+ $op_aggregation = "AND";
+my $rule4 = fuzzy_aggregation_step($op_aggregation ,$fuzzy_service_medium,$fuzzy_food_rancid);
+ print " rule4 = $rule4\n";
 my $weight_rule4 = 1;
+
+ $op_aggregation = "AND";
+my $rule5 = fuzzy_aggregation_step($op_aggregation ,$fuzzy_service_medium,$fuzzy_food_medium);
+ print " rule5 = $rule5\n";
+my $weight_rule5 = 1;
+
+ $op_aggregation = "AND";
+my $rule6 = fuzzy_aggregation_step($op_aggregation ,$fuzzy_service_medium,$fuzzy_food_delicious);
+ print " rule6 = $rule6\n";
+my $weight_rule6 = 1;
+
+ $op_aggregation = "AND";
+my $rule7 = fuzzy_aggregation_step($op_aggregation ,$fuzzy_service_excellent,$fuzzy_food_rancid);
+ print " rule7 = $rule7\n";
+my $weight_rule7 = 1;
+
+ $op_aggregation = "AND";
+my $rule8 = fuzzy_aggregation_step($op_aggregation ,$fuzzy_service_excellent,$fuzzy_food_medium);
+ print " rule8 = $rule8\n";
+my $weight_rule8 = 1; 
+
+ $op_aggregation = "AND";
+my $rule9 = fuzzy_aggregation_step($op_aggregation ,$fuzzy_service_excellent,$fuzzy_food_delicious);
+ print " rule9 = $rule9\n";
+my $weight_rule9 = 1;
 
 ###############  IMPLICATION  :   "OR" OPERATOR  ###############################
 
 # Define the operator for Implication : ("MIN" , "PROD")
 
-my $Op_Implication = "MIN";
+my $op_implication = "MIN";
 
 # Implication phase
 
+my $rule1_result_fuzzyset = fuzzy_implication_step($op_implication, $tip_cheap, ($weight_rule1 * $rule1));
 
-my $rule1_result_fuzzyset = Fuzzy_Implication_Step($Op_Implication, $Maturity_Mature, ($weight_rule1 * $rule1));
+my $rule2_result_fuzzyset = fuzzy_implication_step($op_implication, $tip_cheap, ($weight_rule2 * $rule2));
 
-my $rule2_result_fuzzyset = Fuzzy_Implication_Step($Op_Implication, $Maturity_SemiMature, ($weight_rule2 * $rule2));
+my $rule3_result_fuzzyset = fuzzy_implication_step($op_implication, $tip_average, ($weight_rule3 * $rule3));
 
-my $rule3_result_fuzzyset = Fuzzy_Implication_Step($Op_Implication, $Maturity_Mature, ($weight_rule3 * $rule3));
+my $rule4_result_fuzzyset = fuzzy_implication_step($op_implication, $tip_cheap, ($weight_rule4 * $rule4));
 
-my $rule4_result_fuzzyset = Fuzzy_Implication_Step($Op_Implication, $Maturity_SemiMature, ($weight_rule4* $rule4));
+my $rule5_result_fuzzyset = fuzzy_implication_step($op_implication, $tip_average, ($weight_rule5 * $rule5));
 
+my $rule6_result_fuzzyset = fuzzy_implication_step($op_implication, $tip_average, ($weight_rule6 * $rule6));
 
+my $rule7_result_fuzzyset = fuzzy_implication_step($op_implication, $tip_average, ($weight_rule7 * $rule7));
+
+my $rule8_result_fuzzyset = fuzzy_implication_step($op_implication, $tip_generous, ($weight_rule8 * $rule8));
+
+my $rule9_result_fuzzyset = fuzzy_implication_step($op_implication, $tip_generous, ($weight_rule9 * $rule9)); 
 
 ###############  Accumulation  : Aggregate All Outputs ###############################
 
 # Define the operator for  Accumulation : ("MAX" , "SUM")
 
-my $Op_Accumulation = "MAX";
+my $op_accumulation = "MAX";
 
 #  Accumulation phase
 
+my $tip_result_fuzzyset = initialize_fuzzyterm(0);
 
-my $MATURITY_result_fuzzyset = initialize_fuzzyterm(0);
+  $tip_result_fuzzyset = fuzzy_accumulation_step($op_accumulation,$tip_result_fuzzyset,$rule1_result_fuzzyset);
 
+  $tip_result_fuzzyset = fuzzy_accumulation_step($op_accumulation,$tip_result_fuzzyset,$rule2_result_fuzzyset);
 
-my $MATURITY_result_fuzzyset = Fuzzy_Accumulation_Step($Op_Accumulation,$MATURITY_result_fuzzyset,$rule1_result_fuzzyset);
+  $tip_result_fuzzyset = fuzzy_accumulation_step($op_accumulation,$tip_result_fuzzyset,$rule3_result_fuzzyset);
 
-my $MATURITY_result_fuzzyset = Fuzzy_Accumulation_Step($Op_Accumulation,$MATURITY_result_fuzzyset,$rule2_result_fuzzyset);
+  $tip_result_fuzzyset = fuzzy_accumulation_step($op_accumulation,$tip_result_fuzzyset,$rule4_result_fuzzyset);
 
-my $MATURITY_result_fuzzyset = Fuzzy_Accumulation_Step($Op_Accumulation,$MATURITY_result_fuzzyset,$rule3_result_fuzzyset);
+  $tip_result_fuzzyset = fuzzy_accumulation_step($op_accumulation,$tip_result_fuzzyset,$rule5_result_fuzzyset);
 
-my $MATURITY_result_fuzzyset = Fuzzy_Accumulation_Step($Op_Accumulation,$MATURITY_result_fuzzyset,$rule4_result_fuzzyset);
+  $tip_result_fuzzyset = fuzzy_accumulation_step($op_accumulation,$tip_result_fuzzyset,$rule6_result_fuzzyset);
 
+  $tip_result_fuzzyset = fuzzy_accumulation_step($op_accumulation,$tip_result_fuzzyset,$rule7_result_fuzzyset);
+
+  $tip_result_fuzzyset = fuzzy_accumulation_step($op_accumulation,$tip_result_fuzzyset,$rule8_result_fuzzyset);
+
+ $tip_result_fuzzyset = fuzzy_accumulation_step($op_accumulation,$tip_result_fuzzyset,$rule9_result_fuzzyset);
 
 ###############  Defuzzification  ###############################
 
 # Define the operator for  Defuzzification : ("CG")
 
-my $Op_Defuzzification = "CG";
+my $op_defuzzification = "CG";
 
 #  Defuzzification phase
 
+my $tip_result_crisp = fuzzy_defuzzification_step($op_defuzzification, $tip_result_fuzzyset);
 
-my $MATURITY_result_crisp = Fuzzy_Defuzzification_Step($Op_Defuzzification, $MATURITY_result_fuzzyset);
+ print " tip_result_crisp = $tip_result_crisp\n";
 
-print " MATURITY_result_crisp = $MATURITY_result_crisp\n";
-
-my  $MATURITY = Transform_Step2__RANGE($min_range, $max_range, $MATURITY_result_crisp);
+my $tip = transform_step2__range($min_range, $max_range, $tip_result_crisp);
 
 ##########################################################################################################
 ##########################################################################################################
@@ -255,7 +317,7 @@ my  $MATURITY = Transform_Step2__RANGE($min_range, $max_range, $MATURITY_result_
 # Generate_Values_Membership_Form
 
 
-sub Generate_Values_Membership_Form
+sub generate_values_membership_form
  {
  
    
@@ -264,6 +326,8 @@ my $fuzzyterm  = shift;
 my $prev_key   = 0;
     
 my $prev_value = 0;
+my $intervall;
+my $value_new;
     
 my $return     = initialize_fuzzyterm(0);
     
@@ -314,7 +378,7 @@ return $return;
 
 
 
-sub Transform_Step1_RANGE 
+sub transform_step1_range
 { 
    
 my $min       = shift;
@@ -327,7 +391,7 @@ my $return;
     
 foreach my $key (sort {$a <=> $b} keys %$fuzzyterm) 
 {
-        my $new_key = ScalingToTheRange($min,$max,$key);
+        my $new_key = scaling_to_the_range($min,$max,$key);
 
         $return->{$new_key} = $fuzzyterm->{$key};
    
@@ -340,11 +404,11 @@ return $return;
 
 
 
-# return a pointer  
+# returns a pointer  
  
 
 ########################################
-sub Transform_Step2__RANGE 
+sub transform_step2__range 
 { 
 
    
@@ -404,7 +468,7 @@ for (my $i=0; $i<=$range; $i++)
 
 use Switch;
 
-sub Fuzzy_Aggregation_Step 
+sub fuzzy_aggregation_step 
 { 
 
 # operators to introduce ("AND","OR", "PROD")    
@@ -473,7 +537,7 @@ for (my $i = 0; $i<@_; $i++)
 
 
 
-sub Fuzzy_Implication_Step
+sub fuzzy_implication_step
 
 { 
 
@@ -531,7 +595,7 @@ else { print "ERROR OPERATOR !!" }
 
 ##########################################################################################################
 
-sub Fuzzy_Accumulation_Step
+sub fuzzy_accumulation_step
 
 { 
 
@@ -588,7 +652,7 @@ else { print "ERROR OPERATOR !!" }
 
 ##########################################################################################################
 
-sub Fuzzy_Defuzzification_Step
+sub fuzzy_defuzzification_step
 { 
 
 # method ("CG")  : Center of Gravity  
@@ -596,7 +660,8 @@ sub Fuzzy_Defuzzification_Step
 my $operator       = shift;
     
 my $fuzzyterm     = shift;
-    
+   
+my $return;
  
 if ($operator eq "CG")
 
@@ -638,9 +703,9 @@ else {
 sub round 
 {
   
-  $float = shift;
+  my $float = shift;
   
-  $intgr = int($float + 0.5);
+  my $intgr = int($float + 0.5);
   
   return $intgr;
 
@@ -718,7 +783,7 @@ sub min
 ##########################################################################################################
 
 
-sub ScalingToTheRange 
+sub scaling_to_the_range 
 { 
 
    
